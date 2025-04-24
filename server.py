d41896197ba7e1e9f37ecbe14c79d666c27b446c
busttech <tarun1940c@gmail.com>
2025-04-24 14:21:18 +0530

    hjg

--- server.py
+++ server.py
@@ -3,13 +3,15 @@ import util
 
 app = Flask(__name__)
 
+# 🔧 Load artifacts before any requests are handled
+util.load_saved_artifacts()
+
 @app.route('/get_location_names', methods=['GET'])
 def get_location_names():
     response = jsonify({
         'locations': util.get_location_names()
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
-
     return response
 
 @app.route('/predict_home_price', methods=['GET', 'POST'])
@@ -23,10 +25,8 @@ def predict_home_price():
         'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
-
     return response
 
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
-    util.load_saved_artifacts()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
